9f5e527220e2ac6b0fcdbea5034eb62f0bd4742f
Mindhk92 <hendrikkarsimin@gmail.com>
2015-01-29 17:21:16 +0700

    Comment int PlayerActivity removed

--- app/src/main/java/com/cic/hk/dhammatalk/PlayerActivity.java
+++ app/src/main/java/com/cic/hk/dhammatalk/PlayerActivity.java
@@ -1,45 +1,24 @@
 package com.cic.hk.dhammatalk;
 
-import android.app.ActionBar;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.media.MediaPlayer;
-import android.media.ThumbnailUtils;
-import android.net.Uri;
-import android.os.PersistableBundle;
-import android.provider.MediaStore;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.MediaController;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
-import com.google.android.gms.internal.le;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-import java.io.ByteArrayOutputStream;
-
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-
 
 public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener,
         YouTubePlayer.OnFullscreenListener {
@@ -63,7 +42,6 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_player);
         baseLayout = (LinearLayout) findViewById(R.id.player_activity_main_layout);
         otherViews = findViewById(R.id.other_views);
@@ -77,32 +55,19 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
         youTubeView.initialize(DEVELOPER_KEY, this);
 
         doLayout();
-//
-//        ImageView MyPreview = (ImageView) findViewById(R.id.MyPreview);
-//        Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(this.videoURL, MediaStore.Video.Thumbnails.MICRO_KIND);
-//
-////        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-////        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-//
-//        MyPreview.setImageBitmap(bitmap);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_player, menu);
         return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -126,7 +91,6 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RECOVERY_DIALOG_REQUEST) {
-// Retry initialization if user performed a recovery action
             getYouTubePlayerProvider().initialize(DEVELOPER_KEY, this);
         }
     }
@@ -137,7 +101,6 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
     public void onInitializationSuccess(YouTubePlayer.Provider provider,
                                         YouTubePlayer player, boolean wasRestored) {
         this.YPlayer = player;
-        // Specify that we want to handle fullscreen behavior ourselves.
         player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
         player.setOnFullscreenListener(this);
         if (!wasRestored) {
@@ -155,15 +118,11 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
         LinearLayout.LayoutParams playerParams =
                 (LinearLayout.LayoutParams) youTubeView.getLayoutParams();
         if (fullscreen) {
-            // When in fullscreen, the visibility of all other views than the player should be set to
-            // GONE and the player should be laid out across the whole screen.
             playerParams.width = LinearLayout.LayoutParams.MATCH_PARENT;
             playerParams.height = LinearLayout.LayoutParams.MATCH_PARENT;
 
             otherViews.setVisibility(View.GONE);
         } else {
-            // This layout is up to you - this is just a simple example (vertically stacked boxes in
-            // portrait, horizontally stacked in landscape).
             otherViews.setVisibility(View.VISIBLE);
             ViewGroup.LayoutParams otherViewsParams = otherViews.getLayoutParams();
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
